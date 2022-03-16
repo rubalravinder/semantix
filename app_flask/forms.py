a555b53b3c86a3534a395874d9d6f88053b15369
rubalravinder <rubal.ravinder@gmail.com>
2022-03-16 14:55:52 +0100

    merge conflict resolved

--- app_flask/forms.py
+++ app_flask/forms.py
@@ -2,6 +2,7 @@ import pandas as pd
 import random
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
+from flask_table import Table, Col
 
 def load_vocab_fr():
     '''This function loads our french dictionnary and transforms it as a list'''
@@ -19,15 +20,16 @@ def pick_random_word(mots_fr):
 class SimilarityForm(FlaskForm):
     text = TextAreaField("")
     submit = SubmitField("Send")
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
 
-class SimilarityForm(FlaskForm):
-    text = TextAreaField("")
-    submit = SubmitField("Send")
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+# Declare table
+class Historique(Table):
+    id = Col('id')
+    mot = Col('mot')
+    score = Col('score')
 
-class SimilarityForm(FlaskForm):
-    text = TextAreaField("")
-    submit = SubmitField("Send")
+# Get some objects
+class Proposition(object):
+    def __init__(self, id, mot, score):
+        self.id = id
+        self.mot = mot
+        self.score = score
