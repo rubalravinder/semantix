7d738b094fab20e00b6c8b4925f2915ecc0c45f8
GARRIGOS <martin.garrigos38@gmail.com>
2022-03-16 13:51:24 +0100

    modele leger ajout

--- app_flask/app.py
+++ app_flask/app.py
@@ -2,14 +2,28 @@ from numpy import NaN
 from flask import Flask, request, render_template
 from gensim.models import KeyedVectors
 import warnings
+from forms import *
+from forms import SimilarityForm
+import os
+
+
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim') # not sure it's useful there
 
 
 app = Flask(__name__)
 
+ # generate a secret key for the similarity form (html page test_similarity)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
 # Let's load the Word Embeddings model trained with the .bin composed of tons of french words.
 # Can't really explain how it's working for the moment. 
-model = KeyedVectors.load_word2vec_format("trained_model_french.bin", binary=True, unicode_errors="ignore")
+model = KeyedVectors.load_word2vec_format("../Data/model_leger.bin", binary=True, unicode_errors="ignore")
+vocab_fr = load_vocab_fr() # We load the french dictionnary 
+word_picked = pick_random_word(vocab_fr) # We generate the random french word
+print(word_picked)
+
 
 #####################################################################################################
 ############################ The model is heavy ################################################
@@ -24,10 +38,49 @@ def welcome():
 
 @app.route("/similarity", methods=["GET", "POST"])
 def similarity_route():
-    word1 = 'chien' # We'll have to fix word1 as a random french word
+    word1 = word_picked # We'll have to fix word1 as a random french word
+    print(word1)
     word2 = request.values.get("mot")  # We get the users's word
-    result = model.similarity(word1, word2) # The function which calculate similarities    
-    return render_template('./similarity.html', resultat = result)
+    if word2 not in vocab_fr : 
+        return render_template('./error_word.html')
+    else : 
+        result = model.similarity(word1, word2) # The function which calculate similarities 
+        if word1 == word2 : 
+            return render_template('./win.html')   
+        else : 
+            return render_template('./similarity.html', resultat = result)
+
+@app.route("/test_similarity", methods=["GET", "POST"])
+def similarity_score():
+    form = SimilarityForm()
+
+    if request.method == "POST":
+        word2 = request.form["text"]
+        word1 = 'chien'
+
+        result = model.similarity(word1, word2)
+        # return render_template('./similarity.html', resultat = result)
+
+        return f"""<h2>Your Word</h2> <p> {word2} </p> <h2>Your similarity score with {word1}: </h2> <p>{result}</p>"""
+    
+    else:
+        return render_template("/test_similarity.html", form=form)
+
+@app.route("/test_similarity", methods=["GET", "POST"])
+def similarity_score():
+    form = SimilarityForm()
+
+    if request.method == "POST":
+        word2 = request.form["text"]
+        word1 = 'chien'
+
+        result = model.similarity(word1, word2)
+        # return render_template('./similarity.html', resultat = result)
+
+        return f"""<h2>Your Word</h2> <p> {word2} </p> <h2>Your similarity score with {word1}: </h2> <p>{result}</p>"""
+    
+    else:
+        return render_template("/test_similarity.html", form=form)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ app_flask/forms.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import random
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+
+def load_vocab_fr():
+    '''This function loads our french dictionnary and transforms it as a list'''
+    words = pd.read_csv('../Data/liste_francais.txt', encoding = 'latin1', header = None)
+    vocab = words.iloc[:,0].values.tolist()
+    return vocab
+
+
+def pick_random_word(mots_fr):
+    '''This function picks randomly a word from our french dictionnary'''
+    word_to_guess = random.choice(mots_fr)
+    return word_to_guess
+
+
+class SimilarityForm(FlaskForm):
+    text = TextAreaField("")
+    submit = SubmitField("Send")
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+
+class SimilarityForm(FlaskForm):
+    text = TextAreaField("")
+    submit = SubmitField("Send")
