06a66b79e7f48775be75f25f51d114d9145309f7
rubalravinder <rubal.ravinder@gmail.com>
2022-03-17 16:42:39 +0100

    hover indice et abandonner

--- app_flask/app.py
+++ app_flask/app.py
@@ -29,6 +29,7 @@ vocab_fr = load_vocab_fr(model) # We load the french dictionnary
 word_picked = 'table' # We generate the random french word
 print(word_picked)
 list_of_word_picked = [word_picked]
+longueur_mot = 0
 most_similar = 0.65
 id = 0
 propositions = []
@@ -41,8 +42,10 @@ def bouton():
     global word_picked
     global most_similar
     global list_of_word_picked
+    global longueur_mot
     word_picked = pick_random_word(vocab_fr)
     list_of_word_picked.append(word_picked)
+    longueur_mot = len(word_picked)
     most_similar = round(model.most_similar(word_picked)[0][1], 3)
     print(word_picked)
     print(most_similar)
@@ -65,6 +68,7 @@ def similarity_score():
     global vocab_fr
     global most_similar
     global list_of_word_picked
+    global longueur_mot
 
     # Populate table
     table = Historique(propositions)
@@ -84,10 +88,10 @@ def similarity_score():
             propositions_sorted =  sorted(propositions, key=operator.attrgetter('score'), reverse=True)
             table = Historique(propositions_sorted)
             id+=1
-        return render_template("/play.html", form=form, table=table, most = most_similar, previous_word = list_of_word_picked[-2])
+        return render_template("/play.html", form=form, table=table, most = most_similar, previous_word = list_of_word_picked[-2], longueur_mot = longueur_mot)
         
     else:
-        return render_template("/play.html", form=form, table=table, most = most_similar, previous_word = list_of_word_picked[-2])
+        return render_template("/play.html", form=form, table=table, most = most_similar, previous_word = list_of_word_picked[-2], longueur_mot = longueur_mot)
 
 
 
